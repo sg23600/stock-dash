d93e7ef77511efb0c7ba823298b41780dc223b54
sg23600 <sujaygoswami@hotmail.com>
2020-09-19 22:19:55 +0530

    fixed error

--- dashboard.py
+++ dashboard.py
@@ -24,7 +24,7 @@ def get_more(df):
     df['EWA_20'] = df['Close'].ewm(span=20, adjust=False).mean()
     fig = px.scatter(df,
                      x="Date",
-                     y="EWA_4",
+                     y="EWA_20",
                      title="Exponential Moving Average vs Date")
     fig.update_traces(mode='lines+markers')
     return fig
